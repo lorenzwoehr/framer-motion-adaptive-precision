ff5f5e8fee988f7842644d8cb6c6bde5ac79ee3e
Lorenz Wöhr <hello@lorenzwoehr.com>
2025-01-03 14:37:59 +0100

    Added link to GH

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,18 @@ export default function Home() {
   return (
     <div className="fixed select-none w-screen h-screen items-center justify-center flex bg-neutral-50 dark:bg-neutral-800">
       <Calendar />
+      <p className="absolute bottom-8 text-balance text-center text-xs md:text-sm leading-loose text-neutral-500 dark:text-neutral-600">
+        Source code available on{" "}
+        <a
+          href="https://github.com/lorenzwoehr/framer-motion-adaptive-precision"
+          target="_blank"
+          rel="noreferrer"
+          className="font-medium underline underline-offset-4"
+        >
+          GitHub
+        </a>
+        .
+      </p>
     </div>
   );
 }
